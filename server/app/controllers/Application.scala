ce74713f6ca6d89b3d97a1d757502f449012c2a3
Asjal Ahmad <aahmad@trinity.edu>
2023-11-30 15:29:34 -0600

    ooga

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -11,10 +11,6 @@ import models._
 @Singleton
 class Application @Inject() (cc: ControllerComponents)
     extends AbstractController(cc) {
-      
-  def load = Action { implicit request =>
-    Ok(views.html.testing_page())
-  }
 
   implicit val userDataReads = Json.reads[LoginData]
 
@@ -39,8 +35,19 @@ class Application @Inject() (cc: ControllerComponents)
     Redirect(routes.AuthenticationController.loginPage)
   }
 
-  def testing = Action { implicit request =>
-    Ok(views.html.testing_page())
+  def load = Action { implicit request =>
+    // Logic for the 'load' action
+    // This could be a simple redirect, or you might want to load some initial data for your application.
+    // For example, you might want to check if a user is logged in and redirect accordingly.
+    request.session.get("email") match {
+      case Some(email) => 
+        // If the user is logged in, redirect to a dashboard or home page
+        // Redirect(routes.HomeController.dashboard)
+        Ok(views.html.index(SharedMessages.itWorks))
+      case None => 
+        // If the user is not logged in, redirect to the login page or a public home page
+        Redirect(routes.AuthenticationController.loginPage)
+    }
   }
 
 }
